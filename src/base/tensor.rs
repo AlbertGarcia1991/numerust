3a3df91c262333bfbe2e27b52ecd96c6b6e6762d
AlbertGarciaPlaza <albert.plaza@lego.com>
2025-07-25 17:21:37 +0100

    Implemented Tensor init from &array

--- src/base/tensor.rs
+++ src/base/tensor.rs
@@ -1,5 +1,5 @@
 #[derive(Clone)]
-pub struct Tensor {
+struct Tensor {
     data: Vec<f64>,
     shape: Vec<usize>,
     strides: Vec<usize>,
@@ -15,6 +15,27 @@ fn compute_strides(shape: &[usize]) -> Vec<usize> {
 
 fn prod(dims: &[usize]) -> usize {
     dims.iter().copied().product()
+}
+
+impl Tensor {
+    pub fn new<T>(shape: &[usize], values: &[T]) -> Self
+    where
+        T: Into<f64> + Copy,
+    {
+        if prod(shape) != values.len() {
+            panic!("Shape Mismatch");
+        }
+        let mut data: Vec<f64> = Vec::with_capacity(values.len());
+        for &v in values {
+            data.push(v.into());
+        }
+        let strides: Vec<usize> = compute_strides(shape);
+        Self {
+            data,
+            shape: shape.to_vec(),
+            strides,
+        }
+    }
 }
 
 #[cfg(test)]
@@ -60,5 +81,22 @@ mod tests {
         let shape: [usize; 3] = [2, 3, 5];
         let strides: usize = prod(&shape);
         assert_eq!(strides, 30);
+    }
+
+    #[test]
+    fn test_new_tensor_init() {
+        let tensor: Tensor = Tensor::new(&[2, 2], &[1, 2, 3, 4]);
+        assert_eq!(tensor.data, vec![1., 2., 3., 4.]);
+        assert_eq!(tensor.shape, vec![2, 2]);
+        assert_eq!(tensor.strides, vec![2, 1]);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_new_tensor_init_should_panic() {
+        let tensor: Tensor = Tensor::new(&[2, 3], &[1, 2, 3, 4]);
+        assert_eq!(tensor.data, vec![1., 2., 3., 4.]);
+        assert_eq!(tensor.shape, vec![2, 2]);
+        assert_eq!(tensor.strides, vec![2, 1]);
     }
 }
